668c0acc3433497af7fe379519e8b1f8a180dde5
wangjingping-88 <jingping.wang@ucchip.com>
2021-12-15 09:23:36 +0800

    version.py -> .exe

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v0.03"
-#define GIT_UPDATE_VERSION_TIME  "Sat Dec 4 09:09:46 2021"
+#define GIT_TAIL_TAG   "v0.041"
+#define GIT_UPDATE_VERSION_TIME  "Tue Dec 14 18:53:35 2021"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
