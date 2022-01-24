51f7a6092841c6248c2c458aac69de380cb7de03
wangjingping-88 <jingping.wang@ucchip.com>
2022-01-24 18:37:03 +0800

    add elf file

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -3,7 +3,7 @@
 #define _UC_WIOTA_VERSION_H__
 
 #define GIT_TAIL_TAG   "v0.05"
-#define GIT_UPDATE_VERSION_TIME  "Tue Jan 18 17:30:33 2022"
+#define GIT_UPDATE_VERSION_TIME  "Wed Jan 19 16:42:22 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
