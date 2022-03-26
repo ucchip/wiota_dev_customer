bb99f3763935a4a85fc36833adf1aaccaaab7069
unknown <jpwang@UCCHIP.COM>
2022-03-26 11:54:19 +0800

    add version

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -3,7 +3,7 @@
 #define _UC_WIOTA_VERSION_H__
 
 #define GIT_TAIL_TAG   "v0.09"
-#define GIT_UPDATE_VERSION_TIME  "Sat Mar 26 11:16:28 2022"
+#define GIT_UPDATE_VERSION_TIME  "Sat Mar 26 11:35:58 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
