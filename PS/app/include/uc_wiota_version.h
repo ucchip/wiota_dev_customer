b01589d2cdf86e2c6d2d271704c57d1d871ce180
jingping.wang <76737259+wangjingping-88@users.noreply.github.com>
2022-04-18 15:35:05 +0800

    update doc/bin

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -3,7 +3,7 @@
 #define _UC_WIOTA_VERSION_H__
 
 #define GIT_TAIL_TAG   "v0.10_iote"
-#define GIT_UPDATE_VERSION_TIME  "Sat Mar 26 11:54:19 2022"
+#define GIT_UPDATE_VERSION_TIME  "Mon Apr 18 15:08:55 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
