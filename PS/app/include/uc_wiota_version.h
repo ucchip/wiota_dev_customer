f850d03e7ba0e97448a3c2fc3c5e5d122464f7de
jingping.wang <76737259+wangjingping-88@users.noreply.github.com>
2022-06-09 17:02:58 +0800

    update version

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v0.11_iote"
-#define GIT_UPDATE_VERSION_TIME  "Fri May 6 17:29:32 2022"
+#define GIT_TAIL_TAG   "v0.11_iote_flash"
+#define GIT_UPDATE_VERSION_TIME  "Thu Jun 9 16:26:03 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
