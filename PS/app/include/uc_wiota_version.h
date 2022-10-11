d991817c703145866dda6c96c70f5c96da09d69d
jingping.wang <1182784514@qq.com>
2022-10-11 17:36:39 +0800

    update v1.0

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v0.14_iote"
-#define GIT_UPDATE_VERSION_TIME  "Thu Jul 28 09:31:08 2022"
+#define GIT_TAIL_TAG   "v1.0_iote"
+#define GIT_UPDATE_VERSION_TIME  "Wed Sep 21 11:45:30 2022"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
