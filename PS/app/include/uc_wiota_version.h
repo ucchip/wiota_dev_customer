9863abd1204f73b16fea4272e2fe8c4c24eb0074
zjj609 <zjj609@qq.com>
2025-11-18 11:49:55 +0800

    update version.h

--- PS/app/include/uc_wiota_version.h
+++ PS/app/include/uc_wiota_version.h
@@ -2,8 +2,8 @@
 #ifndef _UC_WIOTA_VERSION_H__
 #define _UC_WIOTA_VERSION_H__
 
-#define GIT_TAIL_TAG   "v3.5_iote"
-#define GIT_UPDATE_VERSION_TIME  "Fri Jun 27 16:59:10 2025"
+#define GIT_TAIL_TAG   "v3.5.2_iote"
+#define GIT_UPDATE_VERSION_TIME  "Tue Nov 18 11:48:05 2025"
 #define TIMESTAMP   (__DATE__ " " __TIME__)
 
 #endif
